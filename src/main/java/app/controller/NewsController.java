c051d1ea8671e6585d19795fcbd3c2efffaeb99e
jalil <celil.ahmad@gmail.com>
2020-06-29 17:27:15 +0400

    --news path

--- src/main/java/app/controller/NewsController.java
+++ src/main/java/app/controller/NewsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/news")
 public class NewsController {
 
     private final NewsService newsService;
