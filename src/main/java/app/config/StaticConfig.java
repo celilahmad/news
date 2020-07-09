cc3669f368c460be8b58ad1e80085d3c319e50e5
jalil <celil.ahmad@gmail.com>
2020-07-09 20:30:07 +0400

    --liquibase --static config

--- src/main/java/app/config/StaticConfig.java
+++ src/main/java/app/config/StaticConfig.java
@@ -5,25 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @Configuration
-@EnableWebMvc
-public class StaticConfig extends WebMvcConfigurerAdapter {
+public class StaticConfig implements WebMvcConfigurer {
 
         @Override
         public void addResourceHandlers(ResourceHandlerRegistry registry) {
             registry.addResourceHandler("/resources/**")
-                    .addResourceLocations("/resources/");
+                    .addResourceLocations("/resources/*");
         }
 
-    @Bean
+    /*@Bean
     public ViewResolver viewResolver() {
         UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
         viewResolver.setViewClass(InternalResourceView.class);
         return viewResolver;
-    }
+    }*/
 
 }
